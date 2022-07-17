d0d6ae481caa7d591568e2be15a6aab433d2b67d
Mohammad Khalid Towhidi <khalid.towhidi@gmail.com>
2022-07-17 22:38:24 +0430

    hi

--- mylist/src/Product/java.java
+++ mylist/src/Product/java.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Product;
 
 /**
@@ -9,5 +6,7 @@ package Product;
  * @author Fazel
  */
 public class java {
+   
+    int  main(args [] ){}
     
 }
--- mylist/src/mylist/Mylist.java
+++ mylist/src/mylist/Mylist.java
@@ -6,7 +6,9 @@ public class Mylist {
 
     
     public static void main(String[] args) {
+        System.out.println("Hello world");
         // TODO code application logic here
     }
+    
     
 }
